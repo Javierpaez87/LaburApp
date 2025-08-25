e2593222062f7ebdd86294d680c6e68d46663ed5
Javierpaez87 <paez.javierandres@gmail.com>
2025-08-24 23:53:10 -0300

    Updated CategoryGrid.tsx

--- src/components/CategoryGrid.tsx
+++ src/components/CategoryGrid.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Hammer, Wrench, HardHat, Building, Zap, PaintBucket, Flame, Gauge, Leaf, PenTool as Tool, HeartHandshake, Scissors, Car, ChefHat, Scissors as Scissors2, Ruler, Palette, Home, Calculator, Truck, Heart, Stethoscope, MoreHorizontal } from 'lucide-react';
+import { Hammer, Wrench, HardHat, Building, Zap, PaintBucket, Flame, Gauge, Leaf, PenTool as Tool, HeartHandshake, Scissors, Car, ChefHat, Scissors as Scissors2, Ruler, Palette, Home, Calculator, Truck, Heart, Stethoscope, Dog, MoreHorizontal } from 'lucide-react';
 import { SERVICE_CATEGORIES } from '@/types/service';
 
 interface CategoryGridProps {
@@ -28,7 +28,7 @@ const categoryIcons = {
   'Decoración': Home,
   'Ingeniería': Calculator,
   'Rental': Truck,
-  'Veterinaria': Animal,
+  'Veterinaria': Dog,
   'Salud': Stethoscope,
   'Otros': MoreHorizontal
 };
