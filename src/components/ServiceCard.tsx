5ca3946908f5adab7a33e555a046f98510fbf092
Javierpaez87 <paez.javierandres@gmail.com>
2025-08-25 00:09:33 -0300

    Updated ServiceCard.tsx

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -37,7 +37,7 @@ export const ServiceCard: React.FC<ServiceCardProps> = ({
     'Decoración': Home,
     'Ingeniería': Calculator,
     'Rental': Truck,
-    'Veterinaria': Stethoscope,
+    'Veterinaria': Dog,
     'Salud': Stethoscope,
     'Otros': MoreHorizontal
   };
